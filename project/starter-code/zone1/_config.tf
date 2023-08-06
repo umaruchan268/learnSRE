d463998af0ff6607ba8a599962c8e681f82d9a2a
umaruchan268 <84457632+umaruchan268@users.noreply.github.com>
2023-08-06 11:47:59 +0700

    push
    push

---
+++ project/starter-code/zone1/_config.tf
@@ -0,0 +1,20 @@
+terraform {
+   backend "s3" {
+     bucket = "uyen2608"
+     key    = "terraform/terraform.tfstate"
+     region = "us-east-2"
+   }
+ }
+
+ provider "aws" {
+   region = "us-east-2"
+   
+   default_tags {
+     tags = local.tags
+   }
+ }
+
+ provider "aws" {
+  alias  = "usw1"
+  region = "us-west-1"
+}
